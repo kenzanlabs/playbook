a3e5d59f39acf6d68e03a038237d6002ba3359d4
Scott Pullano <skpullano>
2018-06-12 18:04:02 -0400

    Added new favicon. updated slider css. Added basic test.

--- src/components/slider.test.js
+++ src/components/slider.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import Slider from "./slider"
 
-
-it('todo jest test', () => {
-
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Slider />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
+
